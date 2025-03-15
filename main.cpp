6cdcf5a497fba9001f6ec48596977462215736fe
pashayevan <nairapash@gmail.com>
2025-03-15 11:48:28 +0400

    15

--- main.cpp
+++ main.cpp
@@ -1,24 +1,3 @@
-/*#include <iostream>
-#include <thread>
-#include <vector>
-#include <mutex>
-#include <chrono>
-#include "mtr.h"
-
-int main() {
-    const int numTrains = 5;
-    std::vector<std::thread> train;
-    for (int i=1; i>+ numTrains; ++i) {
-        train.emplace_back(train, i);
-    }
-    for(auto& t:train) {
-        t.join();
-    }
-    std::cout<<"end of the day.\n";
-    return 0;
-}
-*/
-
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -26,6 +5,7 @@ int main() {
 #include <chrono>
 
 std::mutex metroMutex;
+std::mutex depotMutex;
 
 void station(const std::string& stationName, int trainNumber) {
     std::lock_guard<std::mutex> lock(metroMutex);
@@ -39,20 +19,40 @@ void train(int trainNumber, const std::vector<std::string>& route) {
         station(stationName, trainNumber);
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
-    std::cout << "Train " << trainNumber << " has completed its route.\n";
+    std::cout << "Train " << trainNumber << " has completed its route and is returning to the depot.\n";
+}
+
+void returnToDepot(int trainNumber) {
+    std::lock_guard<std::mutex> lock(depotMutex);
+    std::cout << "Train " << trainNumber << " has arrived at the depot.\n";
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout << "Train " << trainNumber << " is now stationed in the depot.\n";
 }
 
 int main() {
-    std::vector<std::string> route1 = {"Icherisheher", "Sahil", "28 May", "Ganjlik", "Nariman Narimanov"};
-    std::vector<std::string> route2 = {"Hazi Aslanov", "Khatai", "28 May", "Memar Ajami", "Nasimi"};
+    std::vector<std::string> redLine = {"Icherisheher", "Sahil", "28 May", "Ganjlik", "Nariman Narimanov"};
+    std::vector<std::string> greenLine = {"Hazi Aslanov", "Khatai", "28 May", "Memar Ajami", "Nasimi"};
+    std::vector<std::string> blueLine = {"Darnagul", "Azadlig Prospekti", "Nizami", "20 Yanvar", "Inshaatchilar"};
 
     std::vector<std::thread> trains;
     for (int i = 1; i <= 6; ++i) {
-        if (i % 2 == 1) {
-            trains.emplace_back(train, i, std::ref(route1));
+        if (i % 3 == 1) {
+            trains.emplace_back(train, i, std::ref(redLine));
+        } else if (i % 3 == 2) {
+            trains.emplace_back(train, i, std::ref(greenLine));
         } else {
-            trains.emplace_back(train, i, std::ref(route2));
+            trains.emplace_back(train, i, std::ref(blueLine));
         }
+    }
+
+    for (auto& t : trains) {
+        t.join();
+    }
+
+    // Return all trains to depot
+    trains.clear(); // Clear the vector to reuse it
+    for (int i = 1; i <= 6; ++i) {
+        trains.emplace_back(returnToDepot, i);
     }
 
     for (auto& t : trains) {
